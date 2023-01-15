debfe25660de0ddb5446398a616a178eb3cd788d
Tyler Wolf <tjwolf0305@protonmail.com>
2023-01-15 02:50:02 -0500

    add stuff

---
+++ README.md
@@ -0,0 +1,1 @@
+# Henri Helps
---
+++ main.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const router = express.Router();
+const app = express();
+const bodyParser = require('body-parser')
+const port = 5000;
+const cohere = require('cohere-ai');
+cohere.init('cMFr8SdxRiBTGp5P7MrtuyAb9ybdNEXxELSDRi9U');
+var path = require('path');
+var myText;
+
+const rl = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+app.get('/', function(req, res){
+    res.render('form');
+ });
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.post('/', function(req, res) {
+      myText = req.body.mytext;
+      console.log(myText);
+      create();
+    });
+
+function create() {
+    (async () => {
+        const response = await cohere.generate({
+            model: '6d88b2b0-5e24-491a-97cd-b7c8ae5604c2-ft',
+            prompt: 'Respond to the prompt and help the student.\n--\nPrompt: ' + myText + '\"\nResponse:"',
+            max_tokens: 100,
+            temperature: 0.9,
+            k: 0,
+            p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 1,
+            stop_sequences: ['"'],
+            return_likelihoods: 'NONE'
+        });
+        final = response.body.generations[0].text;
+        console.log(final);
+        app.get('/result', function(req, res){
+            res.render("result.ejs", {final:final});
+        });
+    })();
+}
+
+
+
+
+app.listen(5000, () => {
+    console.log('Server is up and running on 5000 ...');
+});
